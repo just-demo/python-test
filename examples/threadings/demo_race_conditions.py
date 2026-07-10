0cdcfb6a3f2b9b0255385e4fce1c7a7e2b4a66cf
Anatolii Kryvda <anatolii.kryvda@gmail.com>
2026-07-10 23:42:21 +0300

    Simplify example of race conditions

--- examples/threadings/demo_race_conditions.py
+++ examples/threadings/demo_race_conditions.py
@@ -1,18 +1,21 @@
 import threading
 import time
 
+N_THREADS = 2
+N_INCREMENTS_PER_THREAD = 1000
+
 counter = 0
 
 
-def worker():
+def increment():
     global counter
-    for _ in range(1000):
+    for _ in range(N_INCREMENTS_PER_THREAD):
         c = counter
-        time.sleep(0.001)
+        time.sleep(0)  # yield execution
         counter = c + 1
 
 
-threads = [threading.Thread(target=worker) for _ in range(2)]
+threads = [threading.Thread(target=increment) for _ in range(N_THREADS)]
 
 for t in threads:
     t.start()
@@ -20,5 +23,5 @@ for t in threads:
 for t in threads:
     t.join()
 
-# prints 1000 instead of 4000
-print(counter)
+print("Expected:", N_THREADS * N_INCREMENTS_PER_THREAD)
+print("Actual:  ", counter)
